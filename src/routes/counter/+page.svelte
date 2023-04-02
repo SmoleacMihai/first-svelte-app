8ddb39e890bfb54afe5cc003e89174c73ed5f6fb
Mihai <mihaismoleac2002@gmail.com>
2023-04-02 22:32:21 +0300

    a litle conter experiment

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  let counter:number = 12
+
+  const increment = () => {
+    counter++
+  }
+
+  const incrementTen = () => {
+    counter += 10
+  }
+
+  const decrement = () => {
+    counter--
+  } 
+  
+  const decrementTen = () => {
+    counter -=10
+  }
+</script>
+
+<div class="counter">
+  <button on:click={increment} on:dblclick={incrementTen}>+</button>
+  <div>{counter}</div>
+  <button on:click={decrement} on:dblclick={decrementTen}>-</button>
+</div>
+
+<style>
+  .counter {
+    display: flex;
+    justify-content: center;
+  }
+
+  .counter > * {
+    padding: 10px;
+  }
+</style>
